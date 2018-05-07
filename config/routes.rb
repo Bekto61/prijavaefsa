69423c6df08e4f4b9c3e9aa5c593d19395352ad3
efsa <mit@efsa.unsa.ba>
2018-05-07 14:12:00 +0200

    added pages and navbar
    added pages and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
  root 'pages#landing_page'
- 
+ get '/o_nama' => 'pages#o_nama'
+ get '/novosti' => 'pages#novosti'
+ get '/kontakt' => 'pages#kontakt'
+
  
 end
